df1d8574d488f6a085b0631b3f6dfe7b403b2e3c
MarkDiamant <markdiamant@gmail.com>
2026-09-07 21:02:34 +0100

    Add Edible Print app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Edible Print | Custom Edible Icing Prints',description:'Custom edible icing prints for cakes, cupcakes, biscuits and branded treats. Fast UK dispatch.'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
